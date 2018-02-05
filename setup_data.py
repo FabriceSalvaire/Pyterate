cb18a206477064936dc1e53cd5647a107f8e72e2
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2018-02-05 22:47:23 +0100

    Implemented version

--- setup_data.py
+++ setup_data.py
@@ -21,6 +21,16 @@
 ####################################################################################################
 
 import os
+import re
+
+####################################################################################################
+
+init_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Pyterate', '__init__.py')
+with open(init_path) as fh:
+    try:
+        version = re.findall(r"^__version__ = '([^']+)'\r?$", fh.read(), re.M)[0]
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
 
 ####################################################################################################
 
@@ -72,7 +82,7 @@ else:
 
 setup_dict = dict(
     name='Pyterate',
-    version='2.0.0',
+    version=version,
     author='Fabrice Salvaire',
     author_email='fabrice.salvaire@orange.fr',
     description='Pyterate is a Sphinx add-ons to generate API documentation using autogen and litterate programming documentation for Python projects.',
