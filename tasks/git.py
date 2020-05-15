e1ec18e47dc16c4845414a98bc3803a95428bc90
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2020-05-16 00:20:17 +0200

    tools -> tasks

---
+++ tasks/git.py
@@ -0,0 +1,35 @@
+####################################################################################################
+#
+# Pyterate - Sphinx add-ons to create API documentation for Python projects
+# Copyright (C) 2020 Fabrice Salvaire
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+####################################################################################################
+
+####################################################################################################
+
+from invoke import task
+
+####################################################################################################
+
+@task
+def push_tags(ctx):
+    ctx.run('git push --tags')
+
+####################################################################################################
+
+@task
+def uncommit(ctx):
+    ctx.run('git reset --soft HEAD~')
