5106a9884d04862f4c3631d86bb3897430f4ebc7
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2020-05-29 16:55:44 +0200

    tasks : doc cleanup

--- tasks/doc.py
+++ tasks/doc.py
@@ -25,16 +25,16 @@ import os
 import shutil
 
 from invoke import task
- # import sys
+# import sys
 
 from .clean import flycheck as _clean_flycheck
 from .release import update_git_sha as _update_git_sha
 
 ####################################################################################################
 
-PYSPICE_SOURCE_PATH = Path(__file__).resolve().parents[1]
+PYTERATE_SOURCE_PATH = Path(__file__).resolve().parents[1]
 
-SPHINX_PATH = PYSPICE_SOURCE_PATH.joinpath('doc', 'sphinx')
+SPHINX_PATH = PYTERATE_SOURCE_PATH.joinpath('doc', 'sphinx')
 BUILD_PATH = SPHINX_PATH.joinpath('build')
 SOURCE_PATH = SPHINX_PATH.joinpath('source')
 API_PATH = SOURCE_PATH.joinpath('api')
@@ -48,13 +48,14 @@ def clean_build(ctx):
     if BUILD_PATH.exists():
         shutil.rmtree(BUILD_PATH)
 
+####################################################################################################
+
 @task
 def clean_api(ctx):
     # ctx.run('rm -rf {}'.format(API_PATH))
     if API_PATH.exists():
         shutil.rmtree(API_PATH)
 
-####################################################################################################
 
 # Fixme: fails if setenv.sh
 
@@ -63,11 +64,14 @@ def clean_api(ctx):
 def make_api(ctx):
     print('\nGenerate RST API files')
     ctx.run('pyterate-rst-api {0.Package}'.format(ctx))
-    print('\nRun Sphinx')
-    with ctx.cd('doc/sphinx/'):
-        ctx.run('make-html') #--clean
+    run_sphinx(ctx)
 
 ####################################################################################################
+
+@task()
+def clean_examples(ctx):
+    shutil.rmtree(EXAMPLES_PATH)
+
 
 # _update_git_sha
 @task()
@@ -75,16 +79,15 @@ def make_examples(ctx, clean=False, no_html=False, force=False):
 
     # Regenerate from scratch
     if clean and EXAMPLES_PATH.exists():
-        shutil.rmtree(EXAMPLES_PATH)
+        clean_examples(ctx)
 
     # pyterate --skip-external-figure --skip-figure
     # PYTHONPATH=$PWD/examples/:${PYTHONPATH}
-    # PySpiceLogLevel=WARNING
+    # PyterateLogLevel=WARNING
 
-    os.environ['PySpiceLibraryPath'] = str(PYSPICE_SOURCE_PATH.joinpath('examples', 'libraries'))
-    os.environ['PySpiceLogLevel'] = 'ERROR'
+    # os.environ['PyterateLogLevel'] = 'ERROR'  # set logging level
 
-    setting_path = PYSPICE_SOURCE_PATH.joinpath('examples', 'Settings.py')
+    setting_path = PYTERATE_SOURCE_PATH.joinpath('examples', 'Settings.py')
     # subprocess.run(('pyterate',
     #                 '--config', str(setting_path))
     # )
@@ -98,12 +101,18 @@ def make_examples(ctx, clean=False, no_html=False, force=False):
     ctx.run(' '.join(command))
 
     if not no_html:
-        print('Run Sphinx')
-        working_path = PYSPICE_SOURCE_PATH.joinpath('doc', 'sphinx')
-        # subprocess.run(('make-html'), cwd=working_path)
-        # --clean
-        with ctx.cd(str(working_path)):
-            ctx.run('make-html')
+        run_sphinx(ctx)
+
+####################################################################################################
+
+@task
+def run_sphinx(ctx):
+    print('\nRun Sphinx')
+    working_path = SPHINX_PATH
+    # subprocess.run(('make-html'), cwd=working_path)
+    # --clean
+    with ctx.cd(str(working_path)):
+        ctx.run('make-html')
 
 ####################################################################################################
 
@@ -112,8 +121,8 @@ def make_readme(ctx):
     from setup_data import long_description
     with open('README.rst', 'w') as fh:
         fh.write(long_description)
-    # import subprocess
-    # subprocess.call(('rst2html', 'README.rst', 'README.html'))
+        # import subprocess
+        # subprocess.call(('rst2html', 'README.rst', 'README.html'))
     ctx.run('rst2html README.rst README.html')
 
 ####################################################################################################
@@ -122,3 +131,9 @@ def make_readme(ctx):
 def update_authors(ctx):
     # Keep authors in the order of appearance and use awk to filter out dupes
     ctx.run("git log --format='- %aN <%aE>' --reverse | awk '!x[$0]++' > AUTHORS")
+
+####################################################################################################
+
+@task
+def xdg_open(ctx):
+    ctx.run('xdg-open doc/sphinx/build/html/index.html')
