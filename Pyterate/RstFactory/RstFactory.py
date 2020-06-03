ea397cb8f41291bf89b16f5c511c9581cca8441c
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2020-06-03 23:13:09 +0200

    workflows/pyterate-test.yml: debug

--- Pyterate/RstFactory/RstFactory.py
+++ Pyterate/RstFactory/RstFactory.py
@@ -134,6 +134,8 @@ class RstFactory:
                 self._logger.warning('Dumped failures in {}'.format(failure_path))
             return False
         else:
+            self._logger.info(str(failure_path))
             if failure_path.exists():
                 failure_path.unlink(missing_ok=True)
+                self._logger.info('HERE')
             return True
